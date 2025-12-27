cfb1b3e9668dddaeb9c78ceb10e3ca3eac8b50ce
Plan C <hubenchang0515@outlook.com>
2025-12-27 11:52:01 +0800

    路径调整

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const worker = new Worker("worker.js", {type: 'module'});
+const worker = new Worker("/js/worker.js", {type: 'module'});
 
 export async function invoke(script, input) {
     return new Promise((resolve) => {
@@ -11,16 +11,13 @@ async function encrypt() {
     const inputText = document.getElementById("input").value;
     const outputText = await invoke("/python/encrypt.py", inputText);
     document.getElementById("output").value = outputText;
-    console.log(inputText, outputText)
 }
 
 async function decrypt() {
     const inputText = document.getElementById("input").value;
     const outputText = await invoke("/python/decrypt.py", inputText);
     document.getElementById("output").value = outputText;
-    console.log(inputText, outputText)
 }
 
 document.getElementById("encrypt").addEventListener("click", encrypt);
 document.getElementById("decrypt").addEventListener("click", decrypt);
-        
--- utils.js
+++
@@ -1,5 +0,0 @@
-// 加载文件，返回内容
-export async function load(url) {
-    const response = await fetch(url);
-    return await response.text();
-}
